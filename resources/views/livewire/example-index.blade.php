3b44c877ad624f88da0cfc0c46162febe36f07d8
- <->
2023-02-18 00:31:12 +0600

    ui: minor tweaks to spacing and wrapping

--- resources/views/livewire/example-index.blade.php
+++ resources/views/livewire/example-index.blade.php
@@ -19,24 +19,24 @@
         <table class="alt-rows">
             <thead>
                 <tr>
-                    <th>en</th>
-                    <th>bn</th>
+                    <th class="cell-en">en</th>
+                    <th class="cell-bn">bn</th>
 
-                    <th>Context</th>
+                    <th class="cell-context">Context</th>
 
-                    <th>Source</th>
+                    <th class="cell-source">Source</th>
 
-                    <th>Links</th>
+                    <th class="cell-links">Links</th>
                 </tr>
             </thead>
             <tbody>
 
                 @foreach ($examples as $example)
                     <tr>
-                        <td>{{ $example->en }}</td>
-                        <td>{{ $example->bn }}</td>
+                        <td class="cell-en">{{ $example->en }}</td>
+                        <td class="cell-bn">{{ $example->bn }}</td>
 
-                        <td>
+                        <td class="cell-context">
                             @if ($example->context)
                                 <div>{{ $example->context }}</div>
                             @endif
@@ -45,9 +45,9 @@
                             @endif
                         </td>
 
-                        <td class="text-center">{{ $example->source }}</td>
+                        <td class="cell-source text-center">{{ $example->source }}</td>
 
-                        <td class="text-center">
+                        <td class="cell-links text-center">
                             <span>
                                 @if ($example->link_1)
                                     <a href="{{ $example->link_1 }}">#1</a>
