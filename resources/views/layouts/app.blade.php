4f9ec73e079649590be8b877c17c2337eef7402e
nr072 <26773186+nr072@users.noreply.github.com>
2023-08-11 01:38:21 +0600

    topbar added to all pages

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,12 +13,58 @@
         <link rel="stylesheet" type="text/css" href="/css/app.css">
 
         <style type="text/css">
+            .topbar {
+                background: var(--color-1-dark);
+                box-shadow: 0px 3px 6px #555;
+                position: fixed;
+                width: 100%;
+                left: 0;
+                top: 0;
+                z-index: var(--topbar-z-index);
+            }
+            .topbar ul {
+                padding: 0;
+                text-align: center;
+            }
+            .topbar li {
+                list-style: none;
+                display: inline-block;
+            }
+            .topbar li > a {
+                font-family: sans-serif;
+                font-variant: small-caps;
+                padding: 1em 1.5em;
+                display: inline-block;
+                color: var(--color-1-lightest);
+                letter-spacing: 0.1em;
+            }
+            .topbar li > a:hover {
+                color: var(--color-1-link);
+                background: var(--color-1-lighter);
+            }
             @yield('more-css')
         </style>
 
     </head>
 
     <body class="cream">
+
+        <nav class="topbar">
+            <ul>
+                <li>
+                    <a href="{{ route('words-page') }}">words</a>
+                </li>
+                <li>
+                    <a href="{{ route('groups-page') }}">groups</a>
+                </li>
+                <li>
+                    <a href="{{ route('sentence-index-page') }}">sentence index</a>
+                </li>
+                <li>
+                    <a href="{{ route('sentence-add-page') }}">add sentences</a>
+                </li>
+            </ul>
+        </nav>
 
         @yield('main')
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,25 +1,5 @@
 @extends('layouts.app')
 
 @section('tab-title')
-    cat
-@endsection
-
-@section('more-css')
-    a.temp-nav {
-        margin: 1rem;
-        padding: 0.5rem 0.75rem;
-        border: 1px solid #333;
-    }
-@endsection
-
-
-@section('main')
-
-    <div style="padding: 2em 0;">
-        <a class="temp-nav" href="{{ route('words-page') }}">words</a>
-        <a class="temp-nav" href="{{ route('groups-page') }}">groups</a>
-        <a class="temp-nav" href="{{ route('sentence-index-page') }}">sentence index</a>
-        <a class="temp-nav" href="{{ route('sentence-add-page') }}">add sentences</a>
-    </div>
-
+    ebong
 @endsection
