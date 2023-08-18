nr072/ebong

commit fc1cd592e7ea8a150afb5ccaedc18b3745bbc38b

wip: one sentence can have multiple sentence translations
Changes:

  - A new model called 'SentenceTranslation' has been added. Adding
    multiple sentence translations is only supported in the back-end
    right now. There is no way of doing this on the front-end yet.

  - Only the sentence adder page loads without any errors. Though
    the bulk adder hasn't been updated yet. And the sentence index
    shows errors right now.

  - 'en' and 'bn' fields have been renamed to 'source' and 'target'.
     And new fields have been added for source and target languages.
    Right now they have only one value each manually pre-selected
    on the front-end.

  - The old 'source' field has been renamed to 'project'.

app/Models/Sentence.php
@@ -5,17 +5,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Group;
+use App\Models\SentenceTranslation;
 
 class Sentence extends Model
 {
     use HasFactory;
 
     protected $fillable = [
-        'en',
-        'bn',
+        'text',
+        'lang',
         'context',
         'subcontext',
-        'source',
+        'project',
         'link_1',
         'link_2',
         'link_3',
@@ -27,5 +28,10 @@ class Sentence extends Model
     public function groups()
     {
         return $this->belongsToMany(Group::class);
+    }
+
+    public function senTranses()
+    {
+        return $this->belongsToMany(SentenceTranslation::class);
     }
 }

app/Http/Livewire/SentenceAdder.php
@@ -5,6 +5,7 @@ namespace App\Http\Livewire;
 use Livewire\Component;
 use App\Models\Group;
 use App\Models\Sentence;
+use App\Models\SentenceTranslation;
 use App\Models\Word;
 
 use Illuminate\Support\Facades\Log;
@@ -54,10 +55,10 @@ class SentenceAdder extends Component
 
 
 
-    // List of sources to show in a dropdown.
-    public $sources = [];
+    // List of projects to show in a dropdown.
+    public $projects = [];
 
-    public $canShowSourceDropdown = true;
+    public $canShowProjectDropdown = true;
 
 
 
@@ -69,11 +70,13 @@ class SentenceAdder extends Component
         manually set somewhere below.
     */
     protected $rules = [
-        'inputs.*.en' => 'required|string',
-        'inputs.*.bn' => 'nullable|string',
+        'inputs.*.sourceText' => 'required|string',
+        'inputs.*.sourceLang' => 'required|string|max:6',
+        'inputs.*.targetText' => 'nullable|string',
+        'inputs.*.targetLang' => 'nullable|string|max:6',
         'inputs.*.context' => 'nullable|max:200',
         'inputs.*.subcontext' => 'nullable|max:200',
-        'inputs.*.source' => 'nullable|max:100',
+        'inputs.*.project' => 'nullable|max:100',
         'inputs.*.link1' => 'nullable|max:200',
         'inputs.*.link2' => 'nullable|max:200',
         'inputs.*.link3' => 'nullable|max:200',
@@ -104,19 +107,15 @@ class SentenceAdder extends Component
         foreach ($validatedData['inputs'] as $input) {
 
             /*
-                Note to dev: Don't forget to add new column names to the
-                model's 'fillable' property.
-
-                Warning: Some default values are manually set here. They
-                might need to be changed when relevant migration files are
-                updated.
+                Note to developer: Don't forget to add new column names
+                to the model's 'fillable' property.
             */
             $newSentence = Sentence::create([
-                'en' => trim( $input['en'] ),
-                'bn' => trim( $input['bn'] ),
+                'text' => trim( $input['sourceText'] ),
+                'lang' => trim( $input['sourceLang'] ),
                 'context' => trim( $input['context'] ),
                 'subcontext' => trim( $input['subcontext'] ),
-                'source' => trim( $input['source'] ),
+                'project' => trim( $input['project'] ),
                 'link_1' => trim( $input['link1'] ),
                 'link_2' => trim( $input['link2'] ),
                 'link_3' => trim( $input['link3'] ),
@@ -127,6 +126,20 @@ class SentenceAdder extends Component
 
             // Groups are associated.
             $newSentence->groups()->attach( $validatedData['chosenGroupIds'] );
+
+            // A sentence translation is created and associated with the
+            // sentence.
+            /*
+                TODO
+                Note to developer: Multiple sentence translations will
+                probably be supported in the future. For now, a single
+                one is being created manually.
+            */
+            $newSenTrans = SentenceTranslation::create([
+                'text' => trim( $input['targetText'] ),
+                'lang' => trim( $input['targetLang'] ),
+                'sentence_id' => $newSentence->id,
+            ]);
 
         }
 
@@ -182,7 +195,7 @@ class SentenceAdder extends Component
     public function addAnotherSentence()
     {
         $this->insertArrayForNewSentece();
-        $this->toggleSourceDropdown(1);
+        $this->toggleProjectDropdown(1);
     }
 
 
@@ -205,11 +218,13 @@ class SentenceAdder extends Component
         array_push(
             $this->inputs, 
             [
-                'en' => '',
-                'bn' => '',
+                'sourceText' => '',
+                'sourceLang' => 'en',
+                'targetText' => '',
+                'targetLang' => 'bn',
                 'context' => '',
                 'subcontext' => '',
-                'source' => '',
+                'project' => '',
                 'link1' => '',
                 'link2' => '',
                 'link3' => '',
@@ -222,18 +237,18 @@ class SentenceAdder extends Component
 
 
 
-    // An existing source is selected from a dropdown.
-    public function selectSource($index, $source)
+    // An existing project is selected from a dropdown.
+    public function selectProject($index, $project)
     {
-        $this->inputs[$index]['source'] = $source;
-        $this->toggleSourceDropdown(0);
+        $this->inputs[$index]['project'] = $project;
+        $this->toggleProjectDropdown(0);
     }
 
 
 
-    public function toggleSourceDropdown($canShow = 0)
+    public function toggleProjectDropdown($canShow = 0)
     {
-        $this->canShowSourceDropdown = $canShow === 1 ? true : false;
+        $this->canShowProjectDropdown = $canShow === 1 ? true : false;
     }
 
     public function toggleGroupDropdown($canShow = 0)
@@ -250,7 +265,7 @@ class SentenceAdder extends Component
     {
         foreach ($this->inputs as $sentence) {
 
-            $wordsInSentence = explode(' ', strtolower($sentence['en']));
+            $wordsInSentence = explode(' ', strtolower($sentence['sourceText']));
 
             $suggestedGroupIds = [];
 
@@ -340,11 +355,11 @@ class SentenceAdder extends Component
             $this->canShowGroupDropdown = $this->searchedGroup ? true : false;
         }
 
-        if ($whichDropdown === 'source' || $whichDropdown === 'all') {
-            if (method_exists($this->sources, 'count')) {
-                $this->canShowSourceDropdown = $this->sources->count() > 0;
+        if ($whichDropdown === 'project' || $whichDropdown === 'all') {
+            if (method_exists($this->projects, 'count')) {
+                $this->canShowProjectDropdown = $this->projects->count() > 0;
             } else {
-                $this->canShowSourceDropdown = sizeof($this->sources) > 0;
+                $this->canShowProjectDropdown = sizeof($this->projects) > 0;
             }
         }
 
@@ -416,8 +431,8 @@ class SentenceAdder extends Component
 
         $this->applySearchFilters();
 
-        // Existing sources are shown in a dropdown to easily choose from.
-        $this->sources = Sentence::groupBy('source')->pluck('source');
+        // Existing projects are shown in a dropdown to easily choose from.
+        $this->projects = Sentence::groupBy('project')->pluck('project');
 
         return view('livewire.sentence-adder', [
             'allGroups' => $this->allGroups,

app/Models/SentenceTranslation.php
@@ -1,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Sentence;
+
+class SentenceTranslation extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'text',
+        'lang',
+        'sentence_id',
+    ];
+
+    public function sentence()
+    {
+        return $this->belongsTo(Sentence::class);
+    }
+}

database/migrations/2023_03_10_071602_create_sentences_table.php
@@ -18,13 +18,13 @@ class CreateSentencesTable extends Migration
             $table->id();
             $table->timestamps();
 
-            $table->text('en');
-            $table->text('bn')->nullable();
+            $table->text('text');
+            $table->string('lang', 6)->default('en');
 
             $table->string('context', 200)->nullable();
             $table->string('subcontext', 200)->nullable();
 
-            $table->string('source', 100)->nullable();
+            $table->string('project', 100)->nullable();
 
             $table->string('link_1', 200)->nullable();
             $table->string('link_2', 200)->nullable();

database/migrations/2023_08_11_175922_create_sentence_translations_table.php
@@ -1,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\Sentence;
+
+class CreateSentenceTranslationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sentence_translations', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->text('text');
+            $table->string('lang', 6);
+
+            $table->foreignIdFor(Sentence::class)->constrained()
+                ->cascadeOnUpdate()
+                ->restrictOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sentence_translations');
+    }
+}

resources/css/app.css
@@ -543,13 +543,23 @@ td.cell-group {
 
    ================================================================== */
 
-.chosen-assoc-word,
 .chosen-assoc-group {
-    border: 1px solid #aaa;
-    padding: 0.15em 0.35em;
+    border: 1px solid var(--color-1-lighter);
+    padding: 0em 0 0 0.5em;
     margin: 0.15em;
     display: inline-block;
     border-radius: 0.5em;
+    line-height: 1em;
+    background: var(--color-1-lightest);
+}
+
+.chosen-assoc-group button {
+    margin: 0;
+    padding: 0.15em 0.5em;
+    margin-left: 0.15em;
+    border: none;
+    background: var(--color-1-lighter);
+    border-radius: 0 0.5em 0.5em 0;
 }
 
 
@@ -562,10 +572,6 @@ td.cell-group {
     justify-content: space-around;
 }
 
-.new-sentence-fields-wrap > div {
-    width: 50%;
-}
-
 
 
 .new-sentence-fields-wrap .input-label-set {
@@ -574,18 +580,23 @@ td.cell-group {
     align-items: baseline;
 }
 
-.input-label-set > span {
+.input-label-set > span:not(.error) {
     text-transform: uppercase;
     font-size: 0.7em;
     font-family: sans-serif;
     margin-right: 0.5em;
-    width: 6rem;
+    max-width: 6rem;
+    min-width: 5rem;
     white-space: nowrap;
 }
 
 .input-label-set > input[type='text'] {
     padding: 0.15rem 0.5rem;
     width: 100%;
+}
+
+.input-label-set span.error {
+    text-wrap: wrap;
 }
 
 

resources/views/layouts/app.blade.php
@@ -33,7 +33,7 @@
             .topbar li > a {
                 font-family: sans-serif;
                 font-variant: small-caps;
-                padding: 1em 1.5em;
+                padding: 0.85em 1.5em;
                 display: inline-block;
                 color: var(--color-1-lightest);
                 letter-spacing: 0.1em;

commit d7246f102829f32fe3a8ad9f741a90b292ac1069

wip: sentence editor now working again
Issue was caused by commit fc1cd59.

app/Http/Livewire/SentenceEditor.php
@@ -5,6 +5,7 @@ namespace App\Http\Livewire;
 use Livewire\Component;
 use App\Models\Group;
 use App\Models\Sentence;
+use App\Models\SentenceTranslation;
 use App\Models\Word;
 
 use Illuminate\Support\Facades\Log;
@@ -19,7 +20,7 @@ class SentenceEditor extends Component
     public $canShowEditor = false;
 
     // The sentence that's being edited.
-    public $sentence = [];
+    public Sentence $sentence;
 
 
 
@@ -53,10 +54,10 @@ class SentenceEditor extends Component
         values in the relevant migration file change.
     */
     protected $rules = [
-        'sentence.bn' => 'nullable|string',
+        'sentence.translations.*.text' => 'nullable|string',
         'sentence.context' => 'nullable|max:200',
         'sentence.subcontext' => 'nullable|max:200',
-        'sentence.source' => 'nullable|max:100',
+        'sentence.project' => 'nullable|max:100',
         'sentence.link_1' => 'nullable|max:200',
         'sentence.link_2' => 'nullable|max:200',
         'sentence.link_3' => 'nullable|max:200',
@@ -88,7 +89,9 @@ class SentenceEditor extends Component
 
     public function closeEditor()
     {
-        $this->reset();
+        $this->resetExcept('sentence');
+        $this->sentence = new Sentence();
+
         $this->toggleEditor(0);
         $this->emitTo('sentence-index', 'editorClosed');
     }
@@ -107,7 +110,7 @@ class SentenceEditor extends Component
     // list of groups that those words belong to.
     public function autosuggestGroups()
     {
-        $wordsInSentence = explode(' ', strtolower($this->sentence['en']));
+        $wordsInSentence = explode(' ', strtolower($this->sentence['text']));
 
         $suggestedGroupIds = [];
 
@@ -161,7 +164,11 @@ class SentenceEditor extends Component
         // Associated groups are updated.
         $this->sentence->groups()->sync($this->chosenGroupIds);
 
-        $this->reset();
+        // All translations for this sentence are updated.
+        $this->sentence->translations->each->save();
+
+        $this->resetExcept('sentence');
+        $this->sentence = new Sentence();
 
         $this->emitTo('sentence-index', 'sentenceUpdated');
         $this->emitTo('sentence-index', 'editorClosed');
@@ -273,6 +280,9 @@ class SentenceEditor extends Component
     public function mount()
     {
         $this->checkDropdownToggling();
+
+        // Stop the Blade view from throwing the 'non-object' error.
+        $this->sentence = new Sentence();
     }
 
 

app/Models/Sentence.php
@@ -30,7 +30,7 @@ class Sentence extends Model
         return $this->belongsToMany(Group::class);
     }
 
-    public function senTranses()
+    public function translations()
     {
         return $this->hasMany(SentenceTranslation::class);
     }

resources/views/livewire/sentence-editor.blade.php
@@ -2,10 +2,10 @@
 
     <div class="popup">
 
-        <button class="button" style="float: right;" wire:click="closeEditor">&times;</button>
+        <button class="button float-right" wire:click="closeEditor">&times;</button>
 
-        @isset ($sentence->en)
-            <p><i>{{ $sentence->en }}</i></p>
+        @isset ($sentence->text)
+            <p><i>{{ $sentence->text }}</i></p>
         @endisset
 
         <div>
@@ -13,14 +13,14 @@
         <div>
             <input type="text" class="searched-group"
                 wire:model="searchedGroup"
-                wire:keydown.enter="addSentence"
+                wire:keydown.enter="saveUpdates"
             >
 
             {{-- Dropdown that shows groups matching the typed input --}}
             @if ($filteredGroups->count() > 0 && $canShowGroupDropdown)
                 <div class="dropdown">
 
-                    <button class="button" style="float: right;"
+                    <button class="button float-right"
                         wire:click="toggleGroupDropdown(0)"
                     >&times;</button>
 
@@ -54,15 +54,17 @@
             <span class="error">{{ $message }}</span>
         @enderror
 
-        <p>
-            <input type="text"
-                wire:model.lazy="sentence.bn" wire:keydown.enter="saveUpdates"
-                placeholder="Enter bn here"
-            >
-            @error ('sentence.bn')
-                <span class="error">{{ $message }}</span>
-            @enderror
-        </p>
+        @foreach ($sentence->translations as $index => $senTrans)
+            <p>
+                <input type="text"
+                    wire:model="sentence.translations.{{ $index }}.text" wire:keydown.enter="saveUpdates"
+                    placeholder="Enter translation here"
+                >
+                @error ('sentence.translations.'.$index. '.text')
+                    <span class="error">{{ $message }}</span>
+                @enderror
+            </p>
+        @endforeach
 
         <p>
             <input type="text"
@@ -82,10 +84,10 @@
             @enderror
 
             <input type="text"
-                wire:model.lazy="sentence.source" wire:keydown.enter="saveUpdates"
-                placeholder="Enter source name here"
+                wire:model.lazy="sentence.project" wire:keydown.enter="saveUpdates"
+                placeholder="Enter project name here"
             >
-            @error ('sentence.source')
+            @error ('sentence.project')
                 <span class="error">{{ $message }}</span>
             @enderror
         </p>
@@ -133,7 +135,7 @@
                 <span class="error">{{ $message }}</span>
             @enderror
 
-            <textarea class="disp-b mt-2" style="width: 30rem; height: 5rem; font-family: sans-serif; padding: 0.5rem;" 
+            <textarea class="disp-b mt-2 w-3/4 max-w-lg h-20 p-2 font-sans"
                 wire:model="sentence.note"
                 placeholder="Enter note here"
             ></textarea>

resources/views/livewire/sentence-index.blade.php
@@ -118,7 +118,7 @@
 
                                 <div>{{ $sentence->text }}</div>
 
-                                @foreach ($sentence->senTranses as $senTrans)
+                                @foreach ($sentence->translations as $senTrans)
                                     <div>{{ $senTrans->text }}</div>
                                 @endforeach
 